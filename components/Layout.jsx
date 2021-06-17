b500d5b185a6228c81d5dc606d8cd431ef9a0578
Rodrigo Dominguez <rodrigo.dominguez@alumnos.ucn.cl>
2021-06-17 03:01:32 -0400

    feat: add layout

---
+++ components/Layout.jsx
@@ -0,0 +1,81 @@
+import React from "react";
+import Header from "./Header";
+import { Global, css } from "@emotion/react";
+import Head from "next/head";
+
+const Layout = (props) => {
+  return (
+    <>
+      <Global
+        styles={css`
+          :root {
+            --gris: #3d3d3d;
+            --gris2: #6f6f6f;
+            --naranja: #da552f;
+            --gris3: #e1e1e1
+          }
+
+          html {
+            font-size: 62.5%;
+            box-sizing: border-box;
+          }
+
+          *,
+          *:before,
+          *:after {
+            box-sizing: inherit;
+          }
+
+          body {
+            font-size: 1.6rem;
+            line-height: 1.5;
+          }
+
+          h1,
+          h2,
+          h3 {
+            margin: 0 0 2rem 0;
+            line-height: 1.5;
+          }
+
+          h1,h2{
+            font-family: 'Roboto-Slab, serif;
+            font-weight: 700;
+          
+          }
+
+          h3{
+            font-family: 'PT Sans', sans-serif;
+          }
+
+          ul {
+            list-style: none;
+            margin: 0;
+            padding: 0;
+          }
+        `}
+      />
+      <Head>
+        <html lang="es"></html>
+        <title>Copia de productHunt</title>
+        <link
+          rel="stylesheet"
+          href="https://cdnjs.cloudflare.com/ajax/libs/normalize/8.0.1/normalize.min.css"
+          integrity="sha512-NhSC1YmyruXifcj/KFRWoC561YpHpc5Jtzgvbuzx5VozKpWvQ+4nXhPdFgmx8xqexRcpAglTj9sIBWINXa8x5w=="
+          crossorigin="anonymous"
+          referrerpolicy="no-referrer"
+        />
+        <link rel="preconnect" href="https://fonts.gstatic.com" />
+        <link
+          href="https://fonts.googleapis.com/css2?family=PT+Sans:ital,wght@0,400;0,700;1,400;1,700&family=Roboto+Slab:wght@100;200;300;400;500;600;700;800;900&display=swap"
+          rel="stylesheet"
+        />
+        <link href="/static/css/app.css" rel="stylesheet" />
+      </Head>
+      <Header></Header>
+      <main>{props.children}</main>
+    </>
+  );
+};
+
+export default Layout;
--- pages/index.js
+++ pages/index.js
@@ -1,13 +1,12 @@
 import styled from "@emotion/styled";
-
-const Heading = styled.h1`
-  color: red;
-`;
+import Layout from "../components/Layout";
 
 export default function Home() {
   return (
     <div>
-      <Heading>Inicio</Heading>
+      <Layout>
+        <h1>Inicio</h1>
+      </Layout>
     </div>
   );
 }
